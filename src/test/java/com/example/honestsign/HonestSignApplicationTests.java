db70e5b4b0ea8c1847ca256b7c9306d224a94259
igr76 <gritsuk766@gmail.com>
2024-07-07 10:01:14 +1000

    start

---
+++ src/test/java/com/example/honestsign/HonestSignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.honestsign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HonestSignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
